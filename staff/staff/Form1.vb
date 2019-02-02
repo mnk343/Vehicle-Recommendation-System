f0376cd24dbcfd8a555d553180fbc59c77ffdc10
mnk343 <mayank343@gmail.com>
2019-02-02 14:22:45 +0530

    deleted some useless files

---
+++ staff/staff/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim uname As String = ""
+        Dim pword As String
+        Dim username As String = ""
+        Dim pass As String = ""
+
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Plz Fill All the info")
+        Else
+            uname = TextBox1.Text
+            pword = TextBox2.Text
+            Dim querry As String = "Select password From staff where Name= '" & uname & "';"
+            Dim dbsource As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='|DataDirectory|\Vehicle Recommendation System Database.accdb' "
+            Dim conn = New OleDbConnection(dbsource)
+            Dim cmd As New OleDbCommand(querry, conn)
+
+            conn.Open()
+            Try
+                pass = cmd.ExecuteScalar().ToString
+            Catch ex As Exception
+                MsgBox("Username does not exit")
+            End Try
+
+            If (pword = pass) Then
+                MsgBox("Login success")
+                ' Form2.Show()
+
+            Else
+                MsgBox("login Failed")
+                TextBox1.Clear()
+                TextBox2.Clear()
+            End If
+        End If
+
+
+
+    End Sub
+End Class
---
+++ staff/staff/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("staff")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("staff")> 
+<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a143f9cd-0fa0-4cc6-ab36-874880e4cbc5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
