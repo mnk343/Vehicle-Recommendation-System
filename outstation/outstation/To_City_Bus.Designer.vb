17ae6921b34a1c07f7c87a93eae290537905f151
avi6gupta <34081808+avi6gupta@users.noreply.github.com>
2019-02-05 04:04:05 +0530

    blah

--- outstation/outstation/To_City_Bus.Designer.vb
+++ outstation/outstation/To_City_Bus.Designer.vb
@@ -22,8 +22,55 @@ Partial Class To_City_Bus
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.time_lbl = New System.Windows.Forms.Label()
+        Me.clk_lbl = New System.Windows.Forms.Label()
+        Me.to_city_bus_dgv = New System.Windows.Forms.DataGridView()
+        CType(Me.to_city_bus_dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'time_lbl
+        '
+        Me.time_lbl.AutoSize = True
+        Me.time_lbl.Location = New System.Drawing.Point(801, 34)
+        Me.time_lbl.Name = "time_lbl"
+        Me.time_lbl.Size = New System.Drawing.Size(39, 17)
+        Me.time_lbl.TabIndex = 0
+        Me.time_lbl.Text = "Time"
+        '
+        'clk_lbl
+        '
+        Me.clk_lbl.AutoSize = True
+        Me.clk_lbl.Location = New System.Drawing.Point(873, 34)
+        Me.clk_lbl.Name = "clk_lbl"
+        Me.clk_lbl.Size = New System.Drawing.Size(51, 17)
+        Me.clk_lbl.TabIndex = 1
+        Me.clk_lbl.Text = "Label2"
+        '
+        'to_city_bus_dgv
+        '
+        Me.to_city_bus_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.to_city_bus_dgv.Location = New System.Drawing.Point(171, 184)
+        Me.to_city_bus_dgv.Name = "to_city_bus_dgv"
+        Me.to_city_bus_dgv.RowTemplate.Height = 24
+        Me.to_city_bus_dgv.Size = New System.Drawing.Size(695, 267)
+        Me.to_city_bus_dgv.TabIndex = 2
+        '
+        'To_City_Bus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1119, 494)
+        Me.Controls.Add(Me.to_city_bus_dgv)
+        Me.Controls.Add(Me.clk_lbl)
+        Me.Controls.Add(Me.time_lbl)
+        Me.Name = "To_City_Bus"
         Me.Text = "To_City_Bus"
+        CType(Me.to_city_bus_dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents time_lbl As System.Windows.Forms.Label
+    Friend WithEvents clk_lbl As System.Windows.Forms.Label
+    Friend WithEvents to_city_bus_dgv As System.Windows.Forms.DataGridView
 End Class
--- outstation/outstation/To_City_Bus.vb
+++ outstation/outstation/To_City_Bus.vb
@@ -1,3 +1,23 @@
 ﻿Public Class To_City_Bus
 
+    Private Access As New DBControl
+
+    Private Function NotEmpty(text As String) As Boolean
+        Return Not String.IsNullOrEmpty(text)
+    End Function
+
+
+    Private Sub To_City_Bus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        clk_lbl.Text = DateAndTime.Now.DayOfWeek
+        Dim day As String = DateAndTime.Now.DayOfWeek
+
+        Access.AddParam("@day", "%" & day & "%")
+
+        ' RUN QUERY
+        Access.ExecQuery("SELECT Source, Destination,WorkingDays FROM BusData WHERE Type=0 AND WorkingDays LIKE @day")
+        If NotEmpty(Access.Exception) Then MsgBox(Access.Exception) : Exit Sub
+
+        ' FILL DATAGRID
+        to_city_bus_dgv.DataSource = Access.DBDT
+    End Sub
 End Class
