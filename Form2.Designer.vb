6189fddcdc02a48db10517131c465bafdf2947bd
AranyaAryaman <39076870+AranyaAryaman@users.noreply.github.com>
2019-02-05 17:48:33 +0530

    UPLOAD FORM FOR MILESTONE-2

---
+++ Form2.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList2 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList3 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList4 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList5 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList6 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.8!)
+        Me.TextBox1.Location = New System.Drawing.Point(549, 166)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(341, 29)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.8!)
+        Me.TextBox2.Location = New System.Drawing.Point(549, 240)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(341, 29)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.8!)
+        Me.TextBox3.Location = New System.Drawing.Point(549, 316)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(341, 29)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.8!)
+        Me.TextBox5.Location = New System.Drawing.Point(549, 385)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox5.Size = New System.Drawing.Size(341, 29)
+        Me.TextBox5.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.8!)
+        Me.Label1.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Label1.Location = New System.Drawing.Point(203, 166)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 29)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "CAB ID"
+        Me.Label1.UseMnemonic = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.8!)
+        Me.Label2.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Label2.Location = New System.Drawing.Point(203, 240)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 29)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Username"
+        Me.Label2.UseMnemonic = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.8!)
+        Me.Label3.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Label3.Location = New System.Drawing.Point(203, 308)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(197, 29)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Contact Number"
+        Me.Label3.UseMnemonic = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.8!)
+        Me.Label5.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Label5.Location = New System.Drawing.Point(203, 385)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(214, 29)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Current Password"
+        Me.Label5.UseMnemonic = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.8!)
+        Me.Button1.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Button1.Location = New System.Drawing.Point(432, 562)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(263, 48)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "UPDATE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.Button2.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Button2.Location = New System.Drawing.Point(937, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(204, 32)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "NEW PASSWORD"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.8!)
+        Me.Label4.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Label4.Location = New System.Drawing.Point(203, 460)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 29)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Places"
+        Me.Label4.UseMnemonic = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList2
+        '
+        Me.ImageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList2.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList2.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList3
+        '
+        Me.ImageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList3.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList3.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList4
+        '
+        Me.ImageList4.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList4.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList4.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList5
+        '
+        Me.ImageList5.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList5.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList5.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList6
+        '
+        Me.ImageList6.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList6.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList6.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(251, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(639, 97)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.8!)
+        Me.Button3.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Button3.Location = New System.Drawing.Point(61, 562)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(263, 48)
+        Me.Button3.TabIndex = 20
+        Me.Button3.Text = "CLEAR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(876, 468)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(84, 21)
+        Me.CheckBox2.TabIndex = 15
+        Me.CheckBox2.Text = "Gangtok"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(762, 468)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(80, 21)
+        Me.CheckBox3.TabIndex = 16
+        Me.CheckBox3.Text = "Tawang"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(643, 468)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(80, 21)
+        Me.CheckBox1.TabIndex = 14
+        Me.CheckBox1.Text = "Shillong"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(549, 468)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(53, 21)
+        Me.CheckBox4.TabIndex = 17
+        Me.CheckBox4.Text = "City"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(896, 387)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(64, 21)
+        Me.CheckBox5.TabIndex = 21
+        Me.CheckBox5.Text = "Show"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.8!)
+        Me.Button4.ForeColor = System.Drawing.Color.MediumBlue
+        Me.Button4.Location = New System.Drawing.Point(824, 562)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(263, 48)
+        Me.Button4.TabIndex = 22
+        Me.Button4.Text = "BACK"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1153, 622)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList2 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList3 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList4 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList5 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList6 As System.Windows.Forms.ImageList
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+End Class
---
+++ Form2.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Data.OleDb
+Public Class Form2
+    Dim cmdUpdate As New OleDbCommand
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cid As String = ""
+        Dim pword As String = ""
+        Dim username As String = ""
+        Dim pass As String = ""
+        If TextBox1.Text = "" Or TextBox5.Text = "" Then
+            MessageBox.Show("Enter the Details to continue")
+        Else
+            cid = TextBox1.Text
+            pword = TextBox5.Text
+           
+            Dim querry As String = "Select [Password] From [CabData] Where [CabId]= " & cid & ";"
+            Dim dbsource As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\Users\Dell\Downloads\database.accdb'"
+            Dim conn As New OleDbConnection(dbsource)
+            Dim cmd As New OleDbCommand(querry, conn)
+            conn.Open()
+            Try
+                pass = cmd.ExecuteScalar().ToString
+            Catch ex As Exception
+                MessageBox.Show("CabID Doesn't Exist")
+            End Try
+            If pword = pass Then
+                cmdUpdate.CommandText = "UPDATE CabData SET UserName = '" & TextBox2.Text & "',Contact = '" & TextBox3.Text & "',City= " & CheckBox4.Checked & ",Shillong= " & CheckBox1.Checked & ",Tawang= " & CheckBox3.Checked & ",Gangtok= " & CheckBox1.Checked & " Where CabID = " & TextBox1.Text & ";"
+                cmdUpdate.CommandType = CommandType.Text
+                cmdUpdate.Connection = conn
+                cmdUpdate.ExecuteNonQuery()
+                MessageBox.Show("Record Updated Successfully", "Success")
+            Else
+                MessageBox.Show("Wrong Password", "Error")
+            End If
+        End If
+        cmdUpdate.Dispose()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.TextBox2.Text = ""
+        Me.TextBox3.Text = ""
+        Me.TextBox5.Text = ""
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.Checked Then
+            TextBox5.PasswordChar = ""
+        Else
+            TextBox5.PasswordChar = "*"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form3.Show()
+    End Sub
+
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+End Class
