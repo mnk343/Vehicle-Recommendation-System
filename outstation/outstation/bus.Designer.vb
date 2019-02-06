24490bae359056ca1aa8208ed03c823c2dcb4a30
annanyapr <annanyapr@gmail.com>
2019-02-06 18:55:59 +0530

    The final version of the bus service

--- outstation/outstation/bus.Designer.vb
+++ outstation/outstation/bus.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class city
+Partial Class bus
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -27,23 +27,19 @@ Partial Class city
         Me.from_city_lbl = New System.Windows.Forms.Label()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.to_city_lbl = New System.Windows.Forms.Label()
-        Me.frm_header = New System.Windows.Forms.Panel()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.to_header = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.to_pnl = New System.Windows.Forms.Panel()
-        Me.to_bus = New System.Windows.Forms.Button()
-        Me.to_cab = New System.Windows.Forms.Button()
+        Me.to_city_bus_dgv = New System.Windows.Forms.DataGridView()
         Me.from_pnl = New System.Windows.Forms.Panel()
-        Me.from_cab = New System.Windows.Forms.Button()
-        Me.from_bus = New System.Windows.Forms.Button()
+        Me.dgv_from_city_bus = New System.Windows.Forms.DataGridView()
+        Me.time_lbl = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.left_fill_pnl.SuspendLayout()
         Me.Panel3.SuspendLayout()
         Me.Panel2.SuspendLayout()
-        Me.frm_header.SuspendLayout()
-        Me.to_header.SuspendLayout()
         Me.to_pnl.SuspendLayout()
+        CType(Me.to_city_bus_dgv, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.from_pnl.SuspendLayout()
+        CType(Me.dgv_from_city_bus, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'left_fill_pnl
@@ -94,137 +90,96 @@ Partial Class city
         Me.to_city_lbl.Text = "To City"
         Me.to_city_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'frm_header
-        '
-        Me.frm_header.Controls.Add(Me.Label2)
-        Me.frm_header.Location = New System.Drawing.Point(309, 1)
-        Me.frm_header.Name = "frm_header"
-        Me.frm_header.Size = New System.Drawing.Size(827, 74)
-        Me.frm_header.TabIndex = 1
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(346, 27)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(116, 29)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "From City"
-        '
-        'to_header
-        '
-        Me.to_header.Controls.Add(Me.Label1)
-        Me.to_header.Location = New System.Drawing.Point(306, 4)
-        Me.to_header.Name = "to_header"
-        Me.to_header.Size = New System.Drawing.Size(827, 74)
-        Me.to_header.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(359, 21)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(89, 29)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "To City"
-        '
         'to_pnl
         '
-        Me.to_pnl.Controls.Add(Me.to_bus)
-        Me.to_pnl.Controls.Add(Me.to_cab)
-        Me.to_pnl.Location = New System.Drawing.Point(309, 78)
+        Me.to_pnl.Controls.Add(Me.to_city_bus_dgv)
+        Me.to_pnl.Location = New System.Drawing.Point(306, 84)
         Me.to_pnl.Name = "to_pnl"
         Me.to_pnl.Size = New System.Drawing.Size(826, 475)
         Me.to_pnl.TabIndex = 2
         '
-        'to_bus
+        'to_city_bus_dgv
         '
-        Me.to_bus.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.to_bus.Location = New System.Drawing.Point(161, 220)
-        Me.to_bus.Name = "to_bus"
-        Me.to_bus.Size = New System.Drawing.Size(195, 87)
-        Me.to_bus.TabIndex = 2
-        Me.to_bus.Text = "Bus"
-        Me.to_bus.UseVisualStyleBackColor = True
-        '
-        'to_cab
-        '
-        Me.to_cab.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.to_cab.Location = New System.Drawing.Point(480, 220)
-        Me.to_cab.Name = "to_cab"
-        Me.to_cab.Size = New System.Drawing.Size(182, 87)
-        Me.to_cab.TabIndex = 1
-        Me.to_cab.Text = "Cab"
-        Me.to_cab.UseVisualStyleBackColor = True
+        Me.to_city_bus_dgv.AllowUserToAddRows = False
+        Me.to_city_bus_dgv.AllowUserToDeleteRows = False
+        Me.to_city_bus_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.to_city_bus_dgv.Location = New System.Drawing.Point(178, 125)
+        Me.to_city_bus_dgv.MultiSelect = False
+        Me.to_city_bus_dgv.Name = "to_city_bus_dgv"
+        Me.to_city_bus_dgv.ReadOnly = True
+        Me.to_city_bus_dgv.RowTemplate.Height = 24
+        Me.to_city_bus_dgv.Size = New System.Drawing.Size(432, 193)
+        Me.to_city_bus_dgv.TabIndex = 5
         '
         'from_pnl
         '
-        Me.from_pnl.Controls.Add(Me.from_cab)
-        Me.from_pnl.Controls.Add(Me.from_bus)
-        Me.from_pnl.Location = New System.Drawing.Point(310, 81)
+        Me.from_pnl.Controls.Add(Me.dgv_from_city_bus)
+        Me.from_pnl.Location = New System.Drawing.Point(307, 81)
         Me.from_pnl.Name = "from_pnl"
         Me.from_pnl.Size = New System.Drawing.Size(826, 472)
         Me.from_pnl.TabIndex = 0
         '
-        'from_cab
+        'dgv_from_city_bus
         '
-        Me.from_cab.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.from_cab.Location = New System.Drawing.Point(485, 223)
-        Me.from_cab.Name = "from_cab"
-        Me.from_cab.Size = New System.Drawing.Size(195, 87)
-        Me.from_cab.TabIndex = 1
-        Me.from_cab.Text = "Cab"
-        Me.from_cab.UseVisualStyleBackColor = True
+        Me.dgv_from_city_bus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_from_city_bus.Location = New System.Drawing.Point(178, 125)
+        Me.dgv_from_city_bus.Name = "dgv_from_city_bus"
+        Me.dgv_from_city_bus.RowTemplate.Height = 24
+        Me.dgv_from_city_bus.Size = New System.Drawing.Size(435, 196)
+        Me.dgv_from_city_bus.TabIndex = 3
         '
-        'from_bus
+        'time_lbl
         '
-        Me.from_bus.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.from_bus.Location = New System.Drawing.Point(166, 223)
-        Me.from_bus.Name = "from_bus"
-        Me.from_bus.Size = New System.Drawing.Size(195, 87)
-        Me.from_bus.TabIndex = 0
-        Me.from_bus.Text = "Bus"
-        Me.from_bus.UseVisualStyleBackColor = True
+        Me.time_lbl.AutoSize = True
+        Me.time_lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.time_lbl.Location = New System.Drawing.Point(749, 31)
+        Me.time_lbl.Name = "time_lbl"
+        Me.time_lbl.Size = New System.Drawing.Size(89, 29)
+        Me.time_lbl.TabIndex = 4
+        Me.time_lbl.Text = "Label2"
         '
-        'city
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(598, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 29)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "TIME "
+        '
+        'bus
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1138, 555)
-        Me.Controls.Add(Me.to_header)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.to_pnl)
         Me.Controls.Add(Me.from_pnl)
-        Me.Controls.Add(Me.frm_header)
         Me.Controls.Add(Me.left_fill_pnl)
-        Me.Name = "city"
-        Me.Text = "City"
+        Me.Controls.Add(Me.time_lbl)
+        Me.Name = "bus"
+        Me.Text = "BUS"
         Me.left_fill_pnl.ResumeLayout(False)
         Me.Panel3.ResumeLayout(False)
         Me.Panel2.ResumeLayout(False)
-        Me.frm_header.ResumeLayout(False)
-        Me.frm_header.PerformLayout()
-        Me.to_header.ResumeLayout(False)
-        Me.to_header.PerformLayout()
         Me.to_pnl.ResumeLayout(False)
+        CType(Me.to_city_bus_dgv, System.ComponentModel.ISupportInitialize).EndInit()
         Me.from_pnl.ResumeLayout(False)
+        CType(Me.dgv_from_city_bus, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents left_fill_pnl As System.Windows.Forms.Panel
     Friend WithEvents Panel3 As System.Windows.Forms.Panel
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
-    Friend WithEvents frm_header As System.Windows.Forms.Panel
     Friend WithEvents to_pnl As System.Windows.Forms.Panel
-    Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents from_pnl As System.Windows.Forms.Panel
-    Friend WithEvents to_header As System.Windows.Forms.Panel
-    Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents from_city_lbl As System.Windows.Forms.Label
     Friend WithEvents to_city_lbl As System.Windows.Forms.Label
-    Friend WithEvents to_bus As System.Windows.Forms.Button
-    Friend WithEvents to_cab As System.Windows.Forms.Button
-    Friend WithEvents from_cab As System.Windows.Forms.Button
-    Friend WithEvents from_bus As System.Windows.Forms.Button
+    Friend WithEvents dgv_from_city_bus As System.Windows.Forms.DataGridView
+    Friend WithEvents time_lbl As System.Windows.Forms.Label
+    Friend WithEvents to_city_bus_dgv As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
