a8b43912f3403bbd57a510170bffb58d48029694
rohannigam001 <47155075+rohannigam001@users.noreply.github.com>
2019-02-10 19:21:24 +0530

    Add files via upload
    minor change

--- Vehicle-Recommendation-System-aviralWithFrontEnd/outstation/outstation/Form1.Designer.vb
+++ Vehicle-Recommendation-System-aviralWithFrontEnd/outstation/outstation/Form1.Designer.vb
@@ -98,9 +98,9 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.BackColor = System.Drawing.Color.White
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.ForeColor = System.Drawing.Color.Black
         Me.Label1.Location = New System.Drawing.Point(269, 156)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(371, 20)
